127801f570f70cc03a71e34811ad77495a99b456
gedholm <gustav@edholm.se>
2019-01-04 13:10:00 +0100

    skeleton of functions for missing data code

---
+++ missing_data/func.py
@@ -0,0 +1,17 @@
+def read_data(file):
+	"""reads text file and creates matrix with data"""
+	return null
+
+def remove_data(T):
+	"""removes 20 procent of data randomly"""
+	return null
+
+def EM(T_missing):
+	"""iteratively calculates W and sigma, treat missing data as latent variables"""
+	return null
+
+def projection(W, sigma):
+	"""equation 12.49 Bishop, calculates the expected value of the latent variable 
+	returns 2xN-matrix with projected data"""
+	return null
+
