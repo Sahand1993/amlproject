b4daac90df77a431651c3d909f569e5ca1915a69
Julia <julia_katarina.cohen@hotmail.com>
2019-01-04 21:30:46 +0100

    completed read_data function

--- missing_data/func.py
+++ missing_data/func.py
@@ -1,6 +1,19 @@
-def read_data(file):
-	"""reads text file and creates matrix with data"""
-	return null
+
+import numpy as np
+
+def read_data(filename, d=18, n=38):
+	""" reads text file and creates matrix with data
+		input: file name and matrix dimensions
+		output: (transposed) matrix with data as column vectors
+	"""
+	f = open(filename)
+	matrix = np.zeros([n,d])
+
+	for i in range(n):
+		data_point = np.asarray([int(s) for s in f.readline().split()])
+		matrix[i] = data_point
+
+	return np.transpose(matrix)
 
 def remove_data(T):
 	"""removes 20 procent of data randomly"""
@@ -15,8 +28,3 @@ def projection(W, sigma):
 	"""equation 12.49 Bishop, calculates the expected value of the latent variable 
 	returns 2xN-matrix with projected data"""
 	return null
-
-def julias_function(param):
-	return 5
-
-	
---
+++ missing_data/main.py
@@ -0,0 +1,5 @@
+from func import *
+
+f = 'tobomovirus.txt'
+complete_data = read_data(f)
+print(complete_data)
