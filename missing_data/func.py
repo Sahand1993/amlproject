e04bd7e45c8cc4f69e29784014c91f5a6cd30216
Julia <julia_katarina.cohen@hotmail.com>
2019-01-05 12:53:05 +0100

    completed remove_data function for array 0 in T matrix

--- missing_data/func.py
+++ missing_data/func.py
@@ -2,58 +2,65 @@ import numpy as np
 import math
 
 def read_data(filename, d=18, n=38):
-	""" reads text file and creates matrix with data
-		input: file name and matrix dimensions
-		output: (transposed) matrix with data as column vectors
-	"""
-	f = open(filename)
-	matrix = np.zeros([n,d])
+    """ reads text file and creates matrix with data
+        input: file name and matrix dimensions
+        output: (transposed) matrix with data as column vectors
+    """
+    f = open(filename)
+    matrix = np.zeros([n,d])
 
-	for i in range(n):
-		data_point = np.asarray([int(s) for s in f.readline().split()])
-		matrix[i] = data_point
+    for i in range(n):
+        data_point = np.asarray([int(s) for s in f.readline().split()])
+        matrix[i] = data_point
 
-	return np.transpose(matrix)
+    return np.transpose(matrix)
 
-def remove_data(T):
-	"""removes 20 procent of data randomly"""
-	"""extra testkommentar"""
-	return null
+def remove_data(T, num_rmv=8):
+    """removes 20 procent of data randomly"""
+    """extra testkommentar"""
+    D = T.shape[0]
+    N = T.shape[1]
+    arr = T[0]
+    corrupting = np.ones(N)
+    corrupting[0:num_rmv] = np.nan
+    np.random.shuffle(corrupting)
+    prod = arr*corrupting
+    return prod
 
 def calc_mean_T(T_missing):
-	T = T_missing
-	D = T.shape[0]
-	N = T.shape[1]
-	mean = np.zeros(D)
-	for i in range(0, D):
-		mean_i = 0
-		missing_counter = 0
-		for j in range(0, N):
-			t = T[i, j]
-			if math.isnan(t):
-				missing_counter += 1
-			else:
-				mean_i +=t
-		mean[i] = mean_i/(N-missing_counter)
-	return mean
+    T = T_missing
+    D = T.shape[0]
+    N = T.shape[1]
+    mean = np.zeros(D)
+    for i in range(0, D):
+        mean_i = 0
+        missing_counter = 0
+        for j in range(0, N):
+            t = T[i, j]
+            if math.isnan(t):
+                missing_counter += 1
+            else:
+                mean_i +=t
+        mean[i] = mean_i/(N-missing_counter)
+    return mean
 
 
 def EM(T_missing, M):
-	"""iteratively calculates W and sigma, treat missing data as latent variables"""
-	
-	return null
+    """iteratively calculates W and sigma, treat missing data as latent variables"""
+    
+    return null
 
-	T = T_missing
-	D = T.shape()[1]
-	W_init = np.zeros((D, M))
-	sigma2 = 1
-	mu = calc_mean_T(T)
-	M_mat = np.matmul(numpy.transpose(W), W) + sigma2*np.eye(M, M)
-	M_mat_inverse = np.inverse(M_mat)
-	expected_X = np.matmul(np.matmul(M_mat_inverse, np.transpose(W)) , 
-							(T.transpose() - mu).transpose())
+    T = T_missing
+    D = T.shape()[1]
+    W_init = np.zeros((D, M))
+    sigma2 = 1
+    mu = calc_mean_T(T)
+    M_mat = np.matmul(numpy.transpose(W), W) + sigma2*np.eye(M, M)
+    M_mat_inverse = np.inverse(M_mat)
+    expected_X = np.matmul(np.matmul(M_mat_inverse, np.transpose(W)) , 
+                            (T.transpose() - mu).transpose())
 
 def projection(W, sigma):
-	"""equation 12.49 Bishop, calculates the expected value of the latent variable 
-	returns 2xN-matrix with projected data"""
-	return null
+    """equation 12.49 Bishop, calculates the expected value of the latent variable 
+    returns 2xN-matrix with projected data"""
+    return null
--- missing_data/main.py
+++ missing_data/main.py
@@ -2,4 +2,18 @@ from func import *
 
 f = 'tobomovirus.txt'
 complete_data = read_data(f)
-print(complete_data)
+#print(complete_data)
+#print(complete_data.shape)
+"""
+testarr = complete_data[0]
+corrupting = np.ones(38)
+corrupting[0:8] = np.nan
+np.random.shuffle(corrupting)
+prod = testarr*corrupting
+print(testarr)
+print(corrupting)
+print(prod)
+"""
+np.random.seed(2)
+corrupted = remove_data(complete_data)
+print(corrupted)
