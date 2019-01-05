19f0266918eaaf7542a8921f611f62c7e2c2d8a8
gedholm <gustav@edholm.se>
2019-01-05 17:18:30 +0100

    added function for calculating S, W_new and sigma2_new, as well as separated the calculation of shortened t vectors to a separate function

--- missing_data/func.py
+++ missing_data/func.py
@@ -50,24 +50,13 @@ def calc_mean_T(T_missing):
         mean[i] = mean_i/(N-missing_counter)
     return mean
 
-
-def EM(T_missing, M):
-	"""iteratively calculates W and sigma, treat missing data as latent variables"""
-	return null
-
-	T = T_missing
-	T_boole = isnan(T)
-	D = T.shape()[1]
-	W_init = np.zeros((D, M))
-	sigma2 = 1
-	mu = calc_mean_T(T)
-	D = T.shape()[1]
+def get_t_and_mu(T):
 	mu = calc_mean_T(T)
 	data_is_missing = np.any(T_boole)
-
 	if data_is_missing:
 		t_list = []
 		mu_list = []
+		nan_indicies_list = []
 		for i in range(0, N):
 			t_i_missing = T[:, i]
 			nan_indices = []
@@ -78,12 +67,53 @@ def EM(T_missing, M):
 			mu_i_removed = np.delete(mu, nan_indices)
 			t_list.append(t_i_removed)
 			mu_list.append(mu_i_removed)
+			nan_indicies_list.append(nan_indices)
 	else:
 		t_list = []
 		mu_list = []
 		for i in range(0, N):
 			t_list.append(T[:, i])
 			mu_list.append(mu)
+	return t_list, mu_list
+
+def calc_S(t, mu, nan_list, D):
+	N = len(t)
+	S = np.zeros((D, D))
+	for i in range(0, N):
+		t_i = t[i]
+		mu_i = mu[i]
+		t_i = np.insert(t_i, nan_list, 0)
+		mu_i = np.insert(mu_i, nan_list, 0)
+		diff = t_i-mu_i
+		mat = np.matmul(diff, np.transpose(diff))
+		S += mat
+	return S/N
+
+def calc_W_new(S, M, W, sigma2):
+	A = np.matmul(S, W) #SW
+	B = sigma2*np.eye(M) # sigma2*I
+	C = np.matmul(np.inverse(M), np.transpose(W)) #M^(-1)W^T
+	D = np.matmul(S, W) #SW
+	return np.matmul(A, np.inverse(B + np.matmul(C, D)))
+
+def calc_sigma2_new(S, W, M_inv, W_new, D):
+	A = np.matmul(S, W) #SW
+	B = np.matmul(M_inv, W_new) #M^(-1)W_new
+	return 1/D * np.trace(S - np.matmul(A, B))
+
+
+
+def EM(T_missing, M):
+	"""iteratively calculates W and sigma, treat missing data as latent variables"""
+	pass
+
+	T = T_missing
+	T_boole = isnan(T)
+	D = T.shape()[1]
+	W_init = np.zeros((D, M))
+	sigma2 = 1
+	D = T.shape()[1]
+	t_list, mu_list = get_t_and_mu(T)
 	
 	W_init = np.zeros((D, M))
 	sigma2 = 1
@@ -99,7 +129,8 @@ def EM(T_missing, M):
 		expected_XX[:, :, i] = sigma2*M_mat_inverse + np.matmul(expected_X[i], np.transpose(expected_X[i]))
 
 
+
 def projection(W, sigma):
-    """equation 12.49 Bishop, calculates the expected value of the latent variable 
+    """equation 12.48 Bishop, calculates the expected value of the latent variable 
     returns 2xN-matrix with projected data"""
     return null
