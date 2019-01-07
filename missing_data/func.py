19d3a13d9a5b49c8925e998ef0e33a72cb9208f3
gedholm <gustav@edholm.se>
2019-01-07 16:38:55 +0100

    debugging mainly finished, results exist, not sure if results are usable though

--- missing_data/func.py
+++ missing_data/func.py
@@ -1,5 +1,6 @@
 import numpy as np
-import math
+import copy
+
 
 def read_data(filename, d=18, n=38):
     """ reads text file and creates matrix with data
@@ -35,22 +36,21 @@ def remove_data(T, num_rmv=136):
 
 def calc_mean_T(T_missing):
 	""" calculates the mean of the data, if corrupted it gives mean anyway"""
-    T = T_missing
-    D = T.shape[0]
-    N = T.shape[1]
-    mean = np.zeros(D)
-    for i in range(0, D):
-        mean_i = 0
-        missing_counter = 0
-        for j in range(0, N):
-            t = T[i, j]
-            if math.isnan(t):
-                missing_counter += 1
-            else:
-                mean_i +=t
-        mean[i] = mean_i/(N-missing_counter)
-    return np.transpose(mean)
-
+	T = T_missing
+	D = T.shape[0]
+	N = T.shape[1]
+	mean = np.zeros(D)
+	for i in range(0, D):
+	    mean_i = 0
+	    missing_counter = 0
+	    for j in range(0, N):
+	        t = T[i, j]
+	        if np.isnan(t):
+	            missing_counter += 1
+	        else:
+	            mean_i +=t
+	    mean[i] = mean_i/(N-missing_counter)
+	return np.transpose(mean)
 
 def EM_v1(T, M):
     """ T is 18x38 
@@ -132,7 +132,6 @@ def get_t_and_mu(T, D):
 	 the third is a list of lists with the indices at which data were missing in the t-vectors"""
 	T_boole = np.isnan(T)
 	N = T.shape[1]
-	print("N"+str(N))
 	mu = calc_mean_T(T)
 	data_is_missing = np.any(T_boole)
 	if data_is_missing:
@@ -149,6 +148,8 @@ def get_t_and_mu(T, D):
 			nan_indices = nan_indices_list[i]
 			t_i_removed = np.delete(t_i_missing, nan_indices)
 			mu_i_removed = np.delete(mu, nan_indices)
+			t_i_removed = np.reshape(t_i_removed, (t_i_removed.shape[0], 1))
+			mu_i_removed = np.reshape(mu_i_removed, (mu_i_removed.shape[0], 1))
 			t_list.append(t_i_removed)
 			mu_list.append(mu_i_removed)
 	else:
@@ -162,6 +163,7 @@ def get_t_and_mu(T, D):
 
 def calc_S(T, mu, t_list, mu_list, nan_list, D):
 	""" calculates the matrix S"""
+	mu = mu.reshape(D, 1)
 	N = len(t_list)
 	S = np.zeros((D, D))
 	for i in range(0, D):
@@ -170,27 +172,29 @@ def calc_S(T, mu, t_list, mu_list, nan_list, D):
 				T[i, j] = 0
 	for i in range(0, N):
 		t_i = T[:, i]
+		t_i = np.reshape(t_i, (D, 1))
 		mu_i = mu
-		t_i = np.insert(t_i, nan_list[i], 0)
-		mu_i = np.insert(mu_i, nan_list[i], 0)
 		diff = t_i-mu_i
-		mat = np.matmul(diff, np.transpose(diff))
+		mat = np.dot(diff, np.transpose(diff))
 		S += mat
-	return S/N
+	S = S/(N*0.8/2)
+	return S
 
 def calc_W_new(S, W, M_inv, sigma2, M):
 	""" calculates the new version of W"""
 	A = np.matmul(S, W) #SW
 	B = sigma2*np.eye(M) # sigma2*I
 	C = np.matmul(M_inv, np.transpose(W)) #M^(-1)W^T
-	D = np.matmul(S, W) #SW
-	return np.matmul(A, np.linalg.inv(B + np.matmul(C, D)))
+	return np.matmul(A, np.linalg.inv(B + np.matmul(C, A)))
 
-def calc_sigma2_new(S, W, W_new, M_inv, D):
+def calc_sigma2_new(S, W, W_new, M_inv_new, M_inv_old, D):
 	""" calculates the new sigma^2 """
 	A = np.matmul(S, W) #SW
-	B = np.matmul(M_inv, np.transpose(W_new)) #M^(-1)W_new
-	return 1/D * np.trace(S - np.matmul(A, B))
+	B = np.matmul(M_inv_old, np.transpose(W_new)) #M^(-1)W_new^T
+	print(np.trace(S))
+	sigma2 = 1.0/D * np.trace(S - np.matmul(A, B))
+	print(sigma2)
+	return sigma2
 
 def calc_M_inv(W, sigma2, M):
 	""" calculates the inverse of the matrix M given W and sigma2"""
@@ -204,14 +208,21 @@ def calc_M_inv_W_T(W, sigma2, M):
 	M_inv_W_T = np.matmul(M_mat_inverse, np.transpose(W))
 	return M_inv_W_T
 
-def calc_expected_X(M_inv_W_T, t_list, mu_list, M):
+def calc_W_from_nan_index(W, nan_list):
+	W = np.delete(W, nan_list, 0)
+	return W
+
+def calc_expected_X(M_inv, W, t_list, mu_list, nan_list, M):
 	""" calculates the current projections on the principas subspace (the latent variables"""
 	N = len(t_list)
 	expected_X = np.zeros((M, N))
 	for i in range(0, N):
-		x = np.matmul( M_inv_W_T, t_list[i]-mu_list[i])
-		print(x)
-		expected_X[:, i] = x
+		W_i = copy.deepcopy(W)
+		nan_i = nan_list[i]
+		W_i = calc_W_from_nan_index(W_i, nan_i)
+		M_inv_W_T = np.dot(M_inv, np.transpose(W_i))
+		x = np.dot( M_inv_W_T, t_list[i]-mu_list[i])
+		expected_X[:, i] = x.reshape(2)
 	return expected_X
 
 def calc_expected_XX(expected_X, sigma2, M_inv):
@@ -226,29 +237,26 @@ def EM(T_missing, M):
 
 	T = T_missing
 	D = T.shape[0]
-	W_init = np.zeros((D, M))
-	sigma2 = 1
 	mu = calc_mean_T(T)
 	t_list, mu_list, nan_list = get_t_and_mu(T, D)
-	W = 5*np.ones((D, M))
-	sigma2 = 1
+	W = (np.random.rand(D, M) - 0.5) * 10
+	sigma2 = 1.0
 	S = calc_S(T, mu, t_list, mu_list, nan_list, D)
 	M_inv = calc_M_inv(W, sigma2, M)
 	repeat = True
-	max_iter = 5000
+	max_iter = 1000
 	counter = 0
 	while counter < max_iter:
 
 		W_new = calc_W_new(S, W, M_inv, sigma2, M)
 		M_inv_new = calc_M_inv(W_new, sigma2, M)
-		sigma2_new = calc_sigma2_new(S, W, W_new, M_inv_new, M)
+		sigma2_new = calc_sigma2_new(S, W, W_new, M_inv_new, M_inv, D)
 		if abs(sigma2 - sigma2_new) < 0.001:
 			repeat = False
 		W = W_new
 		sigma2 = sigma2_new
 		M_inv = M_inv_new
-		counter+=136
-
+		counter+=1
 	return W, sigma2
 
 	
--- missing_data/main.py
+++ missing_data/main.py
@@ -2,20 +2,38 @@ from func import *
 import matplotlib.pyplot as plt
 
 np.random.seed(1)
-op
+
 f = '../dataset/tobomovirus.txt'
 uncorrupted_data = read_data(f)
 corrupted_data = remove_data(uncorrupted_data)
+corrupted_data_copy = corrupted_data.copy()
 M = 2
-D = uncorrupted_data[:, 1]
+N = uncorrupted_data[0, :].size
+D = uncorrupted_data[:, 1].size
+
+"""corrupted"""
 W, sigma2 = EM(corrupted_data, M)
-
-M_inv_W_T = calc_M_inv_W_T(W, sigma2, M)
-
-t_list, mu_list, nan_list = get_t_and_mu(corrupted_data, D)
-
-expected_X = calc_expected_X(M_inv_W_T, t_list, mu_list, M)
-
+M_inv = calc_M_inv(W, sigma2, M)
+t_list, mu_list, nan_list = get_t_and_mu(corrupted_data_copy, D)
+expected_X = calc_expected_X(M_inv, W, t_list, mu_list, nan_list, M)
+fig1, ax1 = plt.subplots()
 plt.scatter(expected_X[0, :], expected_X[1, :])
+for i in range(0, N):
+    ax1.annotate(str(i), (expected_X[0, i], expected_X[1, i]))
 plt.show()
+fig1.savefig("projection_with_missing_data.pdf", bbox_inches='tight')
+
+
+"""uncorrupted"""
+W, sigma2 = EM(uncorrupted_data, M)
+M_inv = calc_M_inv(W, sigma2, M)
+t_list, mu_list, nan_list = get_t_and_mu(uncorrupted_data, D)
+expected_X = calc_expected_X(M_inv, W, t_list, mu_list, nan_list, M)
+fig2, ax2 = plt.subplots()
+plt.scatter(expected_X[0, :], expected_X[1, :])
+for i in range(0, N):
+    ax2.annotate(str(i), (expected_X[0, i], expected_X[1, i]))
+plt.show()
+fig2.savefig("projection_with_normal_data.pdf", bbox_inches='tight')
+
 
